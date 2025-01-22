02e42bca93f4c97de83e8ad967fd4f276790dd07
aruyaohi <omariontechnologies@gmail.com>
2025-01-22 13:30:35 +0100

    made some new changes

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -62,7 +62,7 @@ export default function Header() {
 
       {/* Mobile Navigation */}
       <div
-        className={`lg:hidden bg-black text-white space-y-4 p-6 transition-all duration-300 ${
+        className={`lg:hidden bg-transparent text-white space-y-4 p-6 transition-all duration-300 ${
           isMenuOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
         }`}
         style={{ overflow: 'hidden' }}
