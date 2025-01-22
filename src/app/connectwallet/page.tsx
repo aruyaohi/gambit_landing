65ef348a0b510567421f969bcd2d384f67957351
aruyaohi <omariontechnologies@gmail.com>
2025-01-22 15:26:49 +0100

    made some changes

--- src/app/connectwallet/page.tsx
+++ src/app/connectwallet/page.tsx
@@ -7,24 +7,26 @@ export default function ConnectWallet() {
       <div className="overflow-hidden bg-[#1e1e1e] min-h-screen flex flex-col">
 
         {/* Content */}
-        <div className="flex-grow flex flex-col items-center justify-center gap-10">
+        <div className="flex-grow flex flex-col items-center justify-center gap-7">
           {/* Centered Image */}
           <Image
             src="/images/wallet.png" // Replace with your desired image URL
             alt="Centered Illustration"
+            width={100}
+            height={100}
             className="h-64 w-auto object-contain"
           />
 
           <div>
-            <h1>
-                Connect Your <span className="text-purple-600">Solana</span>  Wallet to Continue
+            <h1 className="text-3xl font-semibold px-4 text-center">
+                Connect Your <span className="text-[#2c76d1]">Solana</span>  Wallet to Continue
             </h1>
           </div>
         </div>
 
         {/* Connect Wallet Button */}
         <div className="py-10 flex justify-center">
-          <button className="py-3 px-24 bg-[#2c76d1] text-white rounded-lg shadow-lg hover:bg-[#1a4e8c] transition duration-300">
+          <button className='py-5 px-20 border border-white rounded-lg hover:bg-[#191919] hover:text-[#2c76d1] hover:border-[#2c76d1] font-semibold'>
             Connect Wallet
           </button>
         </div>
